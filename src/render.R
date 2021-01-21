95b1502905de900b5e835b751b1cfa6af772b28e
cgottin <c.gottin@gmail.com>
2021-01-21 10:59:14 +0100

    test protein drawing

--- src/render.R
+++ src/render.R
@@ -1,3 +1,7 @@
 library(rmarkdown)
 args = commandArgs(trailingOnly=TRUE)
+
+#report_path <- tempfile(fileext = ".Rmd")
+#file.copy("LRR_structure.Rmd", report_path, overwrite = TRUE)
+
 render("LRR_structure.Rmd", output_format = "html_document", output_file = "LRR_structure.html", params = list(wd=args[1]))
